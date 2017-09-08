eb1cb7a1edc2f56ebc56ab6fc289e84631487d51
njorogedavid13@gmail.com <njorogedavid13@gmail.com>
2017-09-08 14:16:21 +0300

    add gulptask bowerCSS for bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,27 @@
 var gulp = require('gulp')
-var lib = require('bower-files')();
 var uglify = require('gulp-uglify')
 var concat = require('gulp-concat')
+var lib = require('bower-files')({
+  "overrides": {
+    "bootstrap": {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
 
 gulp.task('bowerJS', function() {
   return gulp.src(lib.ext('js').files)
     .pipe(concat('vendor.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('bowerCSS', function() {
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
 });

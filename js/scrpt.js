a6faf7ea31feb44b7efd6f414b8c2b915b8fb96e
njorogedavid13@gmail.com <njorogedavid13@gmail.com>
2017-09-08 17:02:09 +0300

    add search input to the code

--- js/scrpt.js
+++ js/scrpt.js
@@ -5,12 +5,13 @@ function Repo() {
 }
 
 
-Repo.prototype.getRepos = function() {
-  $.get('https://api.github.com/users/DavidNjoroge/repos?access_token=' + apiKey).then(function(response) {
+Repo.prototype.getRepos = function(repoName) {
+  $.get('https://api.github.com/users/' + repoName + '/repos?access_token=' + apiKey).then(function(response) {
 
     dat = JSON.stringify(response)
     data = JSON.parse(dat)
     console.log(data);
+    $('.card').empty()
     for (var i = 0; i < data.length; i++) {
       $('.card').append('<div class="card-header">' + data[i].name + '</div>')
       // $('#asdf').append('<li>' + data[i].name + '</li>')
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -9,10 +9,14 @@ var Repo = require('./../js/scrpt.js').repoModule
 
 
 $(document).ready(function() {
+  $('form').submit(function(event) {
+    event.preventDefault();
 
-  var data = new Repo()
-
-  data.getRepos()
+    var repoName = $('#repoName').val()
+    console.log(repoName);
+    var data = new Repo()
+    data.getRepos(repoName)
+  });
   // appender()
 
 });

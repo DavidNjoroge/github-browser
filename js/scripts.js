6d4ed6af3cce70dd9fdcc9d87d856d823130078d
njorogedavid13@gmail.com <njorogedavid13@gmail.com>
2017-09-08 15:52:01 +0300

    add basic business logic code

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,41 @@
+//var apiKey = require("./../.env").apiKey
+
+var getRepos = function() {
+  $.get('https://api.github.com/users/DavidNjoroge/repos?access_token=' + 'cdd1b801556ff7943246cac4feaaf460fb0fad24').then(function(response) {
+    dat = JSON.stringify(response)
+    data = JSON.parse(dat)
+    console.log(data);
+    // data.forEach(function(elm, i) {
+    //   $('#asdf').append('<li>' + elm[i].name + '</li>')
+    // })
+    for (var i = 0; i < data.length; i++) {
+
+      $('#asdf').append('<li>' + data[i].name + '</li>')
+    }
+  }).fail(function(error) {
+    console.log(error.responseJSON.message);
+  });
+
+};
+
+var appender = function() {
+  for (var i = 0; i < data.length; i++) {
+
+    $('#asdf').append('<li>' + data[i].name + '</li>')
+  }
+}
+
+
+
+
+
+
 $(document).ready(function() {
-  alert('boom')
+
+  getRepos()
+
+
+
+  // appender()
+
 });
